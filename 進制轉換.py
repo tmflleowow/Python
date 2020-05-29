f03cec8471ae2cb3adc3fc8402ae9aa51bd1da83
tmflleowow <tmflleowow@gmail.com>
2020-05-29 12:55:09 +0900

    01_簡介 Python 的整數

---
+++ 進制轉換.py
@@ -0,0 +1,32 @@
+#進制轉換
+
+#10進制
+x = 1101
+print(x)
+
+#2進制
+y = 0b1101
+print(y)
+
+
+#8進制
+z = 0o1101
+print(z)
+
+#16進制
+a = 0x1101
+print(a)
+
+#format(數值, “轉換進制”)
+#“轉換進制”:b , o , d , x
+#char format(int , const char*)
+print(format(0b1101, "d") )
+
+
+#char bin(int)
+
+#char oct(int)
+
+#char hex(int)
+
+
