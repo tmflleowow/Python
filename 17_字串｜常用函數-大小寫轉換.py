e4fdd8674fac68f39de8ba9917c728d409c6e926
tmflleowow <tmflleowow@gmail.com>
2020-06-11 11:43:05 +0900

    #大小寫轉換

---
+++ 17_字串｜常用函數-大小寫轉換.py
@@ -0,0 +1,23 @@
+#大小寫轉換
+'''
+全小寫
+"This is a book".lower()
+
+全大寫
+"This is a book".upper()
+
+句首第一個字是大寫
+"this is a book".capitalize()
+
+單詞第一個字是大寫
+"this is a book".title()
+
+大小寫對調
+"This is a book".swapcase()
+
+'''
+print("全小寫 This is a book".lower())
+print("全大寫 This is a book".upper())
+print("this is .a book 句首第一個字大寫 ".capitalize())
+print("單詞第一個字大寫  this is a book".title())
+print("大小寫對調  ThIs iS a boOk".swapcase())
