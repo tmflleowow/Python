cda948a3c0bab9ca6e245c4548cae65cc37a13bb
tmflleowow <tmflleowow@gmail.com>
2020-06-11 11:44:18 +0900

    #從前找到後 #從後找到前 #是否由特定字眼開頭 #是否由特定字眼結尾 #計算該文字的出現次數

---
+++ 17_字串｜常用函數-搜找與計數.py
@@ -0,0 +1,26 @@
+#搜找與計數
+"""
+#從前找到後
+#找不到會回傳-1
+print("This is a book".find("is"))
+
+#找不到會回傳 ValueError
+print("This is a book".index("is"))
+
+#從後找到前
+
+#找不到會回傳-1
+print("This is a book".rfind("is"))
+
+#找不到會回傳 ValueError
+print("This is a book".rindex("is"))
+
+#是否由特定字眼開頭
+print("This is a book".startswith("This"))
+
+#是否由特定字眼結尾
+print("This is a book".endswith("book"))
+
+#計算該文字的出現次數
+print("This is a book".count("is"))
+"""
