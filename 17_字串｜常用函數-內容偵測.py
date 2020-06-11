70fabd2156fff30d480686b1d20551985c1b6dac
tmflleowow <tmflleowow@gmail.com>
2020-06-11 11:48:11 +0900

    #是否全是小寫 #是否全是大寫 #是否句首第一個字是大寫 #是否空白 #是否只含字母 #是否只含數字 #是否只含字母和數字 #是否為肉眼可看見到文字

--- 17_字串｜常用函數-內容偵測.py
+++ 17_字串｜常用函數-內容偵測.py
@@ -1,28 +1,28 @@
 #內容偵測
-'''
-是否全是小寫
+
+#是否全是小寫
 "book".islower()
 
-是否全是大寫
+#是否全是大寫
 "BOOK".isupper()
 
-是否句首第一個字是大寫
+#是否句首第一個字是大寫
 "Book".istitle()
 
-是否空白
+#是否空白
 "   ".isspace()
 
-是否只含字母
+#是否只含字母
 "book".isalpha()
 
-是否只含數字
+#是否只含數字
 "123".isdigit()
 
-是否只含字母和數字
+#是否只含字母和數字
 "13books".isalnum()
 
-是否為肉眼可看見到文字
+#是否為肉眼可看見到文字
 chr(65).isprintable()
 chr(13).isprintable()
 
-'''
+
